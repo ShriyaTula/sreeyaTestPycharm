9ef2508ad2db7ba20e218de79e9789fd4f2d4928
SreeyaTula <75640171+sreeyatula@users.noreply.github.com>
2022-02-25 12:46:17 -0800

    Update demo1.py

--- demo1.py
+++ demo1.py
@@ -1,1 +1,10 @@
-print ("Hello")
+def vowels(S):
+    vs='aeiou'
+    s=S.lower()
+    count=0
+    for each in s:
+        if each in vs:
+            count+=1
+    return count
+S='Am a bad girl a eiou'
+print(vowels(S))
